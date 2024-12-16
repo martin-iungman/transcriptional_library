9be65531e9a21a3ee76e366b3d70772ec2c2ac72
martin-iungman <martin.iungman@gmail.com>
2024-12-16 18:11:31 -0300

    improve plot

--- Analysis/scripts/qpcr.R
+++ Analysis/scripts/qpcr.R
@@ -7,10 +7,16 @@ qpcr_wide=qpcr%>%select(Cq,Target,Sample, Rep,tech,Gate)%>%
 
 df=qpcr_wide%>%filter(Rep!="rep_1", Gate!=1)
 df=df%>%group_by(Rep,Gate)%>%mutate(mean_GAPDH=mean(GAPDH), delta=2^-(EGFP-mean_GAPDH))
-mean_g2=df_poster%>%filter(Gate==2)%>%select(Rep,delta)%>%group_by(Rep)%>%summarise(mean_g2=mean(delta))  
-df_poster=df_poster%>%left_join(mean_g2)%>%mutate(FC=delta/mean_g2)%>%mutate(Gate=as.factor(as.numeric(Gate)-1))
-p2=df_poster%>%filter(Rep=="rep_2")%>%
-  ggplot(aes(Gate, FC, fill=Gate, col=Gate))+geom_line(linewidth=2)+geom_point(size=5)+geom_point(shape = 1,size=5,colour = "black")+ggtitle("Rep 1")+scale_y_log10()+geom_point()+ylab("")+rcartocolor::scale_color_carto_d(palette="Emrld")+theme(legend.position = "none", text=element_text(size=15))
-p3=df_poster%>%filter(Rep=="rep_3")%>%
-  ggplot(aes(Gate, FC, fill=Gate, col=Gate))+geom_line(linewidth=2)+geom_point(size=5)+geom_point(shape = 1,size=5,colour = "black")+ggtitle("Rep 2")+scale_y_log10()+geom_point()+ylab("")+rcartocolor::scale_color_carto_d(palette="Emrld")+theme(legend.position = "none", text=element_text(size=15))
+mean_g2=df%>%filter(Gate==2)%>%select(Rep,delta)%>%group_by(Rep)%>%summarise(mean_g2=mean(delta))  
+df=df%>%left_join(mean_g2)%>%mutate(FC=delta/mean_g2)%>%mutate(Gate=as.factor(as.numeric(Gate)-1))
+p2=df%>%filter(Rep=="rep_2")%>%
+  ggplot(aes(Gate, FC, fill=Gate, col=Gate))+
+  geom_line(linewidth=2)+geom_point(size=5)+geom_point(shape = 1,size=5,colour = "black")+
+  ggtitle("Rep 1")+scale_y_log10()+geom_point()+ylab("EGFP RNA (Fold change vs Gate 1)")+rcartocolor::scale_color_carto_d(palette="Emrld")+
+  ggpubr::theme_pubr()+theme(legend.position = "none")
+p3=df%>%filter(Rep=="rep_3")%>%
+  ggplot(aes(Gate, FC, fill=Gate, col=Gate))+
+  geom_line(linewidth=2)+geom_point(size=5)+geom_point(shape = 1,size=5,colour = "black")+
+  ggtitle("Rep 2")+scale_y_log10()+geom_point()+ylab("")+rcartocolor::scale_color_carto_d(palette="Emrld")+
+  ggpubr::theme_pubr()+theme(legend.position = "none")
 cowplot::plot_grid(p2,p3) #el posta
